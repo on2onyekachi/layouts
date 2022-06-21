3cbc4c7a335ac5c37d55eed31c6e24232d6dd524
Onyekachi Okwuolisa <kachiokwuolisa@gmail.com>
2022-06-21 13:12:58 +0100

    completed the js first task

---
+++ Javascript/Print/script.js
@@ -0,0 +1,12 @@
+const details = {
+    name: "kachi",
+    heigth: "176.6cm",
+    country: "Nigeria"
+}
+let data = []
+for(let detail in details){
+    alert(details[detail])
+    data.push(detail)
+}
+let output = `<p>${data[0]}: <strong>${details.name}</strong>, ${data[1]}: <strong>${details.heigth}</strong>, ${data[2]}: <strong>${details.country}</strong></p>`;
+document.body.innerHTML = output
